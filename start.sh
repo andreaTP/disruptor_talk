83d7ab5fd25b47f2e90e44e9d94c84bc4126dffc
andreaTP <andrea.peruffo1982@gmail.com>
2016-05-26 12:07:17 +0200

    screen support

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 cd ..
 cd wetty
-./start.sh&
+node app.js -p 4000 > /dev/null 2> /dev/null&
 cd ../disruptor_talk
 
-python -m SimpleHTTPServer
+screen -d -m -S shared
+
+python -m SimpleHTTPServer > /dev/null 2> /dev/null& 
+
+screen -x shared
